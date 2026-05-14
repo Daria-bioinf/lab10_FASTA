31f872c1ce364dff94400c557d95affe383fc420
Daria Piskovtseva <d.piskovtseva@gmail.com>
2026-05-14 11:13:21 +0200

    14/05

--- lab10_FASTA/Form1.Designer.cs
+++ lab10_FASTA/Form1.Designer.cs
@@ -28,20 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonJson = new System.Windows.Forms.Button();
+            this.buttonCsv = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.cartesianChart1 = new LiveCharts.Wpf.CartesianChart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(40, 173);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(190, 404);
+            this.listBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(282, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(951, 249);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(40, 44);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(190, 58);
+            this.buttonLoad.TabIndex = 2;
+            this.buttonLoad.Text = "Wczytaj FASTA";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonJson
+            // 
+            this.buttonJson.Location = new System.Drawing.Point(1274, 519);
+            this.buttonJson.Name = "buttonJson";
+            this.buttonJson.Size = new System.Drawing.Size(190, 58);
+            this.buttonJson.TabIndex = 3;
+            this.buttonJson.Text = "Export JSON";
+            this.buttonJson.UseVisualStyleBackColor = true;
+            this.buttonJson.Click += new System.EventHandler(this.buttonJson_Click);
+            // 
+            // buttonCsv
+            // 
+            this.buttonCsv.Location = new System.Drawing.Point(1274, 405);
+            this.buttonCsv.Name = "buttonCsv";
+            this.buttonCsv.Size = new System.Drawing.Size(190, 58);
+            this.buttonCsv.TabIndex = 4;
+            this.buttonCsv.Text = "Exsport CSV";
+            this.buttonCsv.UseVisualStyleBackColor = true;
+            this.buttonCsv.Click += new System.EventHandler(this.buttonCsv_Click);
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(282, 362);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(951, 227);
+            this.elementHost1.TabIndex = 5;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.cartesianChart1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(775, 452);
+            this.ClientSize = new System.Drawing.Size(1507, 616);
+            this.Controls.Add(this.elementHost1);
+            this.Controls.Add(this.buttonCsv);
+            this.Controls.Add(this.buttonJson);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonJson;
+        private System.Windows.Forms.Button buttonCsv;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private LiveCharts.Wpf.CartesianChart cartesianChart1;
     }
 }
 
--- lab10_FASTA/Form1.cs
+++ lab10_FASTA/Form1.cs
@@ -7,14 +7,163 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using CsvHelper;
+using System.Globalization;
+
+
 
 namespace lab10_FASTA
 {
     public partial class Form1 : Form
     {
+        List<SequenceData> sequenceList = new List<SequenceData>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public class SequenceData
+        {
+            public string Name { get; set; }
+            public int Length { get; set; }
+            public double GC_Content { get; set; }
+            public int A_Count { get; set; }
+            public int T_Count { get; set; }
+            public int G_Count { get; set; }
+            public int C_Count { get; set; }
+            public int Codons { get; set; }
+        }
+
+
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "FASTA files (*.fasta)|*.fasta|Text files (*.txt)|*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] lines = File.ReadAllLines(openFileDialog.FileName);
+
+                if (!lines.Any(line => line.StartsWith(">")))
+                {
+                    MessageBox.Show("Invalid FASTA file!");
+                    return;
+                }
+
+                sequenceList.Clear();
+
+                string currentName = "";
+                string currentSequence = "";
+
+                foreach (string line in lines)
+                {
+                    if (line.StartsWith(">"))
+                    {
+                        if (currentSequence.Length > 0)
+                        {
+                            sequenceList.Add(AnalyzeSequence(currentName, currentSequence));
+                        }
+
+                        currentName = line.Substring(1);
+                        currentSequence = "";
+                    }
+                    else
+                    {
+                        string sequencePart = line.Trim().ToUpper();
+
+                        foreach (char ch in sequencePart)
+                        {
+                            if ("ATGCN".Contains(ch) == false)
+                            {
+                                MessageBox.Show("Invalid DNA sequence!");
+
+                                return;
+                            }
+                        }
+
+                        currentSequence += sequencePart;
+                    }
+                }
+
+                if (currentSequence.Length > 0)
+                {
+                    sequenceList.Add(AnalyzeSequence(currentName, currentSequence));
+                }
+
+                listBox1.Items.Clear();
+
+                foreach (var seq in sequenceList)
+                {
+                    listBox1.Items.Add(seq.Name);                
+                }
+                dataGridView1.DataSource = sequenceList;
+
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            }
+        }
+        private SequenceData AnalyzeSequence(string name, string sequence)
+        {
+            int a = sequence.Count(x => x == 'A');
+            int t = sequence.Count(x => x == 'T');
+            int g = sequence.Count(x => x == 'G');
+            int c = sequence.Count(x => x == 'C');
+
+            int length = sequence.Length;
+            int codons = length / 3;
+
+            double gc = ((double)(g + c) / length) * 100;
+
+            return new SequenceData
+            {
+                Name = name,
+                Length = length,
+                GC_Content = Math.Round(gc, 2),
+                A_Count = a,
+                T_Count = t,
+                G_Count = g,
+                C_Count = c,
+                Codons = codons
+            };
+
+        }
+
+        private void buttonJson_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "JSON files (*.json)|*.json";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string json =
+                    JsonConvert.SerializeObject(sequenceList, Formatting.Indented);
+
+                File.WriteAllText(saveFileDialog.FileName, json);
+
+                MessageBox.Show("JSON exported!");
+            }
+        }
+
+        private void buttonCsv_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "CSV files (*.csv)|*.csv";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (var writer = new StreamWriter(saveFileDialog.FileName))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(sequenceList);
+                }
+
+                MessageBox.Show("CSV exported!");
+            }
         }
     }
 }
